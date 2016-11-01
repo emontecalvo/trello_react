8c75333d758ef5de3db9bf02f47cb72c42f831a1
PaulChong <paulhyunchong@gmail.com>
2016-11-01 12:32:54 -0700

    change naming for array to clear up confusion

--- js/components/board.js
+++ js/components/board.js
@@ -4,24 +4,21 @@ var Card = require('./card');
 var List = require('./list');
 
 var Board = function(props) {
-	var lists = [
+	var listsCard = [
 		{ 
-			title: "List1", 
 			cards: ["First card in the list", "Second card in the list", "Third card in the list"]
 		},
 		{ 	
-			title: "List2",
 			cards: ["1st card in the list", "2nd card in the list", "3rd card in the list"]
 		},
 		{
-			title: "List3",
 			cards: ["First card in the list", "Second card in the list", "Third card in the list"]
 		}
 	]
 	var listsInBoard = [];
 	console.log(props);
-	for (var i = 0; i < lists.length; i++) {
-		listsInBoard.push(<List title={props.lists[i]} cards={lists[i].cards} />);
+	for (var i = 0; i < listsCard.length; i++) {
+		listsInBoard.push(<List title={props.lists[i]} cards={listsCard[i].cards} />);
 	}
 	return (
 		<div className="board-list">
